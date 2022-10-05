5ebe062143f33c04659b3226ba36a236c41f426d
Santiago Fabián Quispe <quispesantiagofabian@gmail.com>
2022-10-04 21:26:13 -0300

    Correción en los Routes. Navegación Funcional.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
-import Home from './routes/Home/Home';
 import Navigation from './routes/Navigation/Navigation';
+import Home from './routes/Home/Home';
 import Add from './routes/Add/Add';
-
 
 function App() {
   return (
     <div className="App">
+      <Navigation/>
       <Routes>
-        <Route path='/' element={<Navigation/>}>
+        <Route path='/'>
           <Route index element={<Home/>}/>
           <Route path='add' element={<Add/>}/>
         </Route>
--- src/routes/Navigation/Navigation.jsx
+++ src/routes/Navigation/Navigation.jsx
@@ -1,17 +1,22 @@
 import './Navigation.css';
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
     return (
         <>
             <div className='navigation'>
+                <div className='nav-title'>
+                    <h1>Júpiter App</h1>
+                </div>
                 <div className='nav-links-container'>
+                    <Link className='nav-link' to='/'>
+                        Home
+                    </Link>
                     <Link className='nav-link' to='/add'>
                         Add
                     </Link>
                 </div>
             </div>
-            <Outlet />
         </>
     );
 };
