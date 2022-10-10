d53457005ebba8c19b2cc16d579bb0dd514bbcb3
Santiago Fabián Quispe <quispesantiagofabian@gmail.com>
2022-10-10 11:24:34 -0300

    Cambios de las direcciones APIs de Open-Meteo. Añadir argumentos en las funciones getDataWeather y getGeocoding de Service.

--- src/service.js
+++ src/service.js
@@ -1,13 +1,14 @@
 /* Direcciones de las APIs de Open-Meteo. */
-const GEOCODING = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&language=es&count=1`;
-// Variable: Nombre de Ciudad
-const WEATHER_FORECAST = `https://api.open-meteo.com/v1/forecast?current_weather=true&latitude=${lat}&longitude=${lon}&timezone=America/Argentina/Jujuy`;
-// Variables: Latitud y Longitud.
+const GEOCODING = `https://geocoding-api.open-meteo.com/v1`;
+const WEATHER_FORECAST = `https://api.open-meteo.com/v1`;
 
 /* Obtener información con Weather Forecast API. */
-export const getDataWeather = async () => {
+export const getDataWeather = async (lat, lon) => {
     try {
-        const response = await fetch(WEATHER_FORECAST);
+        parseFloat(lat);
+        parseFloat(lon);
+        const response = await fetch(`${WEATHER_FORECAST}/forecast?current_weather=true&latitude=${lat}&longitude=${lon}&timezone=America/Argentina/Jujuy`);
+        console.log(response.json());
         return response.json();
     } catch {
         throw new Error('could not fetch weather data');
@@ -15,9 +16,10 @@ export const getDataWeather = async () => {
 };
 
 /* Obtener información con Geocoding API. */
-export const getGeocoding = async () => {
+export const getGeocoding = async (city) => {
     try {
-        const response = await fetch(GEOCODING);
+        const response = await fetch(`${GEOCODING}/search?name=${city}&language=es&count=1`);
+        console.log(response.json());
         return response.json();
     } catch {
         throw new Error('could not fetch city data');
